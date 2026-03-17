eabad69683fdf638eec532bd52a484ebde1e09d0
npt <trangnphe171779@fpt.edu.vn>
2026-03-17 22:51:45 +0700

    modified temporary

--- src/java/controllers/CheckoutController.java
+++ src/java/controllers/CheckoutController.java
@@ -1,10 +1,8 @@
 package controllers;
 
-import dal.DiscountDAO;
 import dal.MenuDAO;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,11 +10,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import models.Discount;
 import models.MenuItem;
 import models.User;
-import models.RestaurantDeliveryZone;
-import dal.RestaurantDeliveryZoneDAO;
 
 @WebServlet(name = "CheckoutController", urlPatterns = {"/checkout"})
 public class CheckoutController extends HttpServlet {
@@ -73,64 +68,9 @@ public class CheckoutController extends HttpServlet {
                 return;
             }
 
-            // Load available discounts for this restaurant (platform-wide or specific)
-            DiscountDAO discountDAO = new DiscountDAO();
-            List<Discount> allDiscounts = discountDAO.getAvailableDiscountsForRestaurant(restaurantId);
-
-            List<Discount> usableVouchers = new java.util.ArrayList<>();
-            List<Discount> unusableVouchers = new java.util.ArrayList<>();
-            Discount bestDiscount = null;
-            double maxDiscountGained = 0;
-
-            for (Discount d : allDiscounts) {
-                boolean isAmountValid = (d.getMinOrderAmount() == null || totalAmount >= d.getMinOrderAmount());
-                boolean isQuantityValid = d.getQuantity() > 0;
-                boolean isUsageValid = true;
-                
-                if (d.getUsageLimitPerUser() > 0) {
-                    int usedCount = discountDAO.countUserUsage(d.getDiscountID(), user.getUserID());
-                    if (usedCount >= d.getUsageLimitPerUser()) {
-                        isUsageValid = false;
-                    }
-                }
-
-                if (isAmountValid && isQuantityValid && isUsageValid) {
-                    usableVouchers.add(d);
-                    
-                    double currentGain = 0;
-                    if ("Percentage".equalsIgnoreCase(d.getDiscountType())) {
-                        currentGain = totalAmount * (d.getDiscountValue() / 100.0);
-                        if (d.getMaxDiscountAmount() != null && d.getMaxDiscountAmount() > 0 && currentGain > d.getMaxDiscountAmount()) {
-                            currentGain = d.getMaxDiscountAmount();
-                        }
-                    } else {
-                        currentGain = d.getDiscountValue();
-                    }
-                    if (currentGain > totalAmount) {
-                        currentGain = totalAmount;
-                    }
-                    
-                    if (currentGain > maxDiscountGained) {
-                        maxDiscountGained = currentGain;
-                        bestDiscount = d;
-                    }
-                } else {
-                    unusableVouchers.add(d);
-                }
-            }
-
             request.setAttribute("cartItems", cartItems);
             request.setAttribute("totalAmount", totalAmount);
             request.setAttribute("restaurantId", restaurantId);
-            request.setAttribute("usableVouchers", usableVouchers);
-            request.setAttribute("unusableVouchers", unusableVouchers);
-            request.setAttribute("bestDiscount", bestDiscount);
-            request.setAttribute("maxDiscountGained", maxDiscountGained);
-            
-            // Get Delivery Zones for this restaurant
-            RestaurantDeliveryZoneDAO zoneDAO = new RestaurantDeliveryZoneDAO();
-            List<RestaurantDeliveryZone> deliveryZones = zoneDAO.getActiveZonesByRestaurantId(restaurantId);
-            request.setAttribute("deliveryZones", deliveryZones);
             
             request.getRequestDispatcher("views/checkout.jsp").forward(request, response);
             
